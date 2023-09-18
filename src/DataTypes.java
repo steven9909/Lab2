56486a490c1775f65317e3b4305ed27487fd4b17
Mani Mehdipoor <mani.mehdipoor@mail.utoronto.ca>
2023-09-18 11:41:33 -0400

    Fixed DataTypes

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -2,9 +2,10 @@ import java.util.List;
 
 public class DataTypes {
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
-    public static long sum(List<Integer> numbers) {
+    public static long sum(List<Integer> numbers)
+    {
+        long s = 0;
 
-        int s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
